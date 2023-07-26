aeabac3da6b272c9f55cec3d2b2c10128a2bb0b6
Princepritz <pritamsinghsolanke35@gmail.com>
2023-07-26 11:54:45 +0530

    second

--- dom.js
+++ dom.js
@@ -1,2 +1,13 @@
-console.log(document.forms);
 
+const head=document.getElementById('header-title');
+head.style.borderBottom="solid 3px #000"
+console.log(head);
+
+var item=document.getElementsByClassName('list-group-item');
+//item[1].style.backgroundColor='red';
+
+for(var i=0;i<item.length;i++){
+    item[i].style.backgroundColor='grey';
+}
+
+//
